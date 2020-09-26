87c12eb4dd6f5ea6eb384d245e519c1ae8568d29
Varun <varunsridharan23@gmail.com>
2020-09-26 17:23:51 +0530

    Updated

--- test/sofy.js
+++ test/sofy.js
@@ -1,15 +1,15 @@
-const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin;
-
 const files = {
-	/*'./src/style/main.scss': [
+	'./src/style/main.scss': [
 		{
 			scss: true,
-			dist: './dist/css/'
+			dist: './dist/css/',
+			watch: true,
 		},
 		{
 			scss: true,
 			autoprefixer: true,
 			minify: true,
+			watch: true,
 			rename: 'main.pio.css',
 			dist: './dist/css/'
 		}
@@ -18,28 +18,33 @@ const files = {
 	'babelJS': {
 		src: './src/js/babel/index.js',
 		babel: true,
+		watch: true,
 		dist: './dist/js/babel/'
 	},
 	'combineFiles': {
 		src: './src/js/combine/combine.js',
 		combine_files: true,
+		watch: true,
 		dist: './dist/js/combine/',
 	},
-	'./src/js/concat/!*.js': {
+	'./src/js/concat/*.js': {
 		dist: 'dist/js',
 		concat: 'concat.js',
+		watch: true,
 		rename: 'concat.js',
-	},*/
+	},
 	'./src/js/webpack/index.js': {
 		dist: 'dist/js/webpack/',
 		webpack: true,
 		uglify: true,
+		watch: true,
 		rename: 'webpack.js',
 	},
 	'pluginsCSS': {
 		src: './src/style/m2.scss',
 		scss: { outputStyle: 'compressed' },
-		dist: './dist/css/'
+		dist: './dist/css/',
+		watch: true,
 	},
 };
 
